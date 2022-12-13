c46eba40d28014e0d9974c073e10506dd50f6365
anita-brown <efeaniita@gmail.com>
2022-12-13 13:15:06 +0100

    chore: about us created

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ const App = () => (
   <div>
     <Navbar />
     <Header />
-    {/* <AboutUs />
-    <SpecialMenu />
-    <Chef />
-    <Intro />
-    <Laurels />
+    <AboutUs />
+    {/* <SpecialMenu /> */}
+    {/* <Chef /> */}
+    {/* <Intro /> */}
+    {/* <Laurels />
     <Gallery />
     <FindUs />
     <Footer /> */}
